1bf565dd9c01a8e84560deea6b9624667923b9b1
fmartin0212 <fmartin0212@gmail.com>
2018-11-09 11:47:59 -0700

    Adds SolarSystem base file

---
+++ Afternoon Projects/Unit 1 - UIKit & Fundamentals/Day 2/SolarSystem/SolarSystem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SolarSystem
+//
+//  Created by Frank Martin Jr on 11/9/18.
+//  Copyright © 2018 DevMtnStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
